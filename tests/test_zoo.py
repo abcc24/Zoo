c887afc3615230bc22ee23a75f5a3bfa4b737010
abcc24 <dirge20@gmail.com>
2024-05-31 18:37:26 +0200

    d

--- tests/test_zoo.py
+++ tests/test_zoo.py
@@ -1,12 +1,20 @@
 import unittest
 from unittest import TestCase
+from src.Zoo import Zoo, Fence, ZooKeeper, Animal
 
 class TestZoo(TestCase):
-    pass
+    def set_up(self) -> None:
+        self.zookeper1: ZooKeeper = ZooKeeper ("Rocco Balocco", 1)
+        self.fence1: Fence = Fence (100, 25, 6, "Foresta")
+        self.animal1: Animal = Animal("Fenrir", "lupo", 5,1,4, "Foresta")
+        self.zoo1: Zoo = Zoo()
 
+    def test_1(self):
 
-
-
+        self.zookeper1.add_animal(self.animal1, self.fence1)
+        result: int = len(self.fence1.animal)
+        message: str = "Error: the function add animal should not add animal into fence1"
+        self.assertEqual(result, 0, message)
 
 
 
